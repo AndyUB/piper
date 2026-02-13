900348fdfaa77243896cc386054f87cf20898fc0
meganfrisella <meganfrisella@gmail.com>
2026-02-13 21:57:10 +0000

    rm files

--- src/test_compile.py
+++
@@ -1,7 +0,0 @@
-from test.models.llama import Transformer, LLAMA_1B
-
-from torch._dynamo.backends.debugging import eager
-
-model = Transformer(LLAMA_1B, 1024)
-dummy = torch.compile(model, backend=eager, fullgraph=True)
-dummy(torch.randint(0, 1024, (1, 1024)))
