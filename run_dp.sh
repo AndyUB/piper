02bf621db86ed28f2f1c9f8ccdfcbca5393036be
Andy Ruan <andyubryh@gmail.com>
2026-02-19 00:16:43 +0000

    scripts and results
    Signed-off-by: Andy Ruan <andyubryh@gmail.com>

---
+++ run_dp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+LOG_DIR=logs
+mkdir -p $LOG_DIR
+# RAY_TMP=$HOME/ray_tmp
+RAY_TMP=/m-coriander/coriander/tmp/ray
+mkdir -p $RAY_TMP
+export RAY_TMPDIR="$RAY_TMP"
+export PYTHONUNBUFFERED=1
+
+export CUDA_VISIBLE_DEVICES=0,1,2
+export RAY_DEDUP_LOGS=0
+export PIPER_MASTER_PORT=12388
+
+# python3 -m test.test_llama --iters 5 --schedule interleaved-1f1b --mbs 4 > $LOG_DIR/interleaved.log 2>&1
+python3 -m test.test_llama \
+    --dp 2 \
+    --warmup 5 \
+    --iters 10 \
+    --schedule interleaved-1f1b \
+    --mbs 4 \
+    --model debug > $LOG_DIR/interleaved_debug.log 2>&1
